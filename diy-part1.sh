4149c6130a5643df192a340d9cc84115481bd2fa
CCA2878 <51525400+CCA2878@users.noreply.github.com>
2022-03-06 12:44:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
