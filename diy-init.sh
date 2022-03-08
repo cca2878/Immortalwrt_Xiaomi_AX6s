44d7923bf42c6a5e553cb84857decc4eebe5ab7f
CCA2878 <51525400+CCA2878@users.noreply.github.com>
2022-03-08 10:51:51 +0800

    Update diy-init.sh

--- diy-init.sh
+++ diy-init.sh
@@ -1,1 +1,3 @@
 curl -s https://build-scripts.immortalwrt.eu.org/init_build_environment.sh | sudo bash
+sudo apt update
+sudo apt install build-essential ccache ecj fastjar file g++ gawk gettext git java-propose-classpath libelf-dev libncurses5-dev libncursesw5-dev libssl-dev python python2.7-dev python3 unzip wget python3-distutils python3-setuptools python3-dev rsync subversion swig time xsltproc zlib1g-dev 
